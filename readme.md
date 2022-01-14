e7e12bf0a47eef32e8b7e5f87f175e231329e869
Denis <jsherr.6@gmail.com>
2022-01-14 18:04:06 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+install Styles Componets 
+https://www.npmjs.com/package/@emotion/react
+https://www.npmjs.com/package/@emotion/styled
+
+1--> npm i @emotion/react
+2--> npm i @emotion/styled
+
+
+En jsx podemos utilizar HTML con expresiones JavaScript, pero
+tambien es posible crear un elemento HTML con la sintaxis de un
+componente y añadir propiedades CSS
+
+en lugar de utilizar una hoja de estilos globales o una libreria externa, 
+se escribe el codigo CSS en cada comonente (CSS en JS)
+
+ventajas:
+  Al dejar de utilizar un componente, podemos eliminar su codigo CSS tambien, 
+  a diferencia de una hoja de estilos global.
+  Sigues teniendo la ventaja de reutilizar codigo CSS
+
+  
---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import styled from "@emotion/styled";
+import ImagenCriptoMoneda from "./img/imagen-criptos.png";
+import Formulario from "./Formulario";
+
+const Contenedor = styled.div`
+  max-width: 120rem;
+  width: 90%;
+  margin-inline: auto;
+`;
+const Main = styled.main`
+  // width: 90%;
+  @media (min-width: 992px) {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-inline: auto;
+  }
+`;
+const Caja = styled.div`
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 6rem;
+  }
+`;
+const Imagen = styled.img`
+  width: 100%;
+  object-fit: cover;
+transition: 0.3s;
+filter: grayscale(50%);
+
+  &:hover {
+    opacity: 100%;
+    /* margin-top: -5px; */
+    transform: scale(0.9);
+    filter: grayscale(0%);
+    cursor:pointer;
+  }
+  @media (min-width: 992px) {
+    height: 43rem;
+  }
+`;
+const Box = styled.div`
+  width: 300px;
+  max-width: 100%;
+  margin-inline: auto;
+  box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
+  border-radius: 2rem;
+  @media (min-width: 992px) {
+    width: 400px;
+    margin-inline: auto;
+  }
+`;
+const Heading = styled.h1`
+  text-align: center;
+  margin: 0 0 3rem 0;
+  &::after {
+    content: "";
+    width: 10rem;
+    height: 0.6rem;
+    background-color: #66a2fe;
+    display: block;
+    margin: 0 auto;
+  }
+`;
+function App() {
+  return (
+    <Contenedor>
+      <Main>
+        <Caja>
+          <div className="Box-margin">
+            <Box>
+              <Imagen
+                src={ImagenCriptoMoneda}
+                alt="Cotiza Criptomoneda al Instante"
+              />
+            </Box>
+          </div>
+          <div>
+            <Heading>Desde APP</Heading>
+            <Formulario />
+          </div>
+        </Caja>
+      </Main>
+    </Contenedor>
+  );
+}
+
+export default App;
